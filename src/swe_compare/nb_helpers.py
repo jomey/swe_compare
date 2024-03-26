5fa7aa66debb20ac4d4146b47d52b600bc7552a4
Joachim Meyer <jmeyer@boisestate.edu>
2024-03-26 16:01:21 -0500

    src - Expand nb_helpers to spin up SSHCluster
    Add an option to start a Dask cluster via SSH

--- src/swe_compare/nb_helpers.py
+++ src/swe_compare/nb_helpers.py
@@ -1,11 +1,33 @@
-from dask.distributed import Client
+import logging
+from dask.distributed import Client, SSHCluster
 
-def start_cluster(n_workers=10, memory_limit='4GB'):
-    cluster = Client(
-        n_workers=n_workers, 
-        threads_per_worker=1, 
-        memory_limit=memory_limit
-    )
+def start_cluster(n_workers=10, memory_limit='4GB', local=True):
+    if local:
+        cluster = Client(
+            n_workers=n_workers,
+            threads_per_worker=1,
+            memory_limit=memory_limit
+        )
+    else:
+        # NOTE:
+        # First host in the list is the scheduler, all others are workers
+        ssh_cluster = SSHCluster(
+            ["localhost", "honduras"],
+            connect_options={
+                "known_hosts": None,
+            },
+            worker_options={
+                "nthreads": 1,
+                "n_workers": n_workers,
+                "memory_limit": memory_limit,
+            },
+            scheduler_options={
+                "port": 0,
+                "dashboard_address": ":8797"
+            }
+        )
+        cluster = Client(ssh_cluster)
+        
     print(cluster.dashboard_link)
     return cluster
-    
+

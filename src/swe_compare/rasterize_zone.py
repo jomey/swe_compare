9c9bf1da7faebcd97605ab0407a0947a1a572ba8
Joachim Meyer <jmeyer@boisestate.edu>
2024-04-16 10:25:45 -0500

    src - rasterize - Expand to support a single zone.
    Expand rasterize helper to convert just a single given zone given
    as a geopandas data frame.

--- src/swe_compare/rasterize_zone.py
+++ src/swe_compare/rasterize_zone.py
@@ -6,13 +6,75 @@ from rasterio import features
 from shapely.geometry import mapping
 
 MASK_NO_ZONE = -999
+RASTERIZE_OPTIONS = dict(
+    all_touched=True,
+    fill=MASK_NO_ZONE,
+    dtype=rasterio.int16
+)
 
 
-def cbrfc_zone_mask(zone_raster, zone_shape):
+def rasterize_cbrfc_zone(zone_raster, zone_geometries):
+    """
+    Convert collection of CBRFC zone into a raster.
+
+    Use rasterio to convert given zone geometries (polygons) into a gridded
+    version defined by the given zone raster.
+
+    TODO: Look into replacing with native GDAL
+          https://gdal.org/api/python/utilities.html#osgeo.gdal.RasterizeLayer
+
+    Parameters
+    ----------
+    zone_raster : String
+        Path to raster file
+    zone_geometries : Iterable
+        List of geometries to convert
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    """
     with rasterio.open(zone_raster) as zone_tif:
         transform = zone_tif.transform
         shape = zone_tif.shape
 
+    return features.rasterize(
+        zone_geometries,
+        transform=transform,
+        out_shape=shape,
+        **RASTERIZE_OPTIONS
+    )
+
+
+def single_cbrfc_zone_mask(zone_raster, zone_gpd):
+    """
+    Convert a single CBRFC zone into a raster.
+
+    This will convert the first row of the given geopandas dataframe only.
+
+    Parameters
+    ----------
+    zone_raster : String
+        Path to raster file
+    zone_gpd : geopandas dataframe
+        Dataframe holding the geometries
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    """
+    zone_geometry = [
+        (mapping(zone_gpd.iloc[0].geometry), 1)
+    ]
+
+    return rasterize_cbrfc_zone(zone_raster, zone_geometry)
+
+
+def cbrfc_zone_mask(zone_raster, zone_shape):
     zone_geometries = (
         (mapping(geometry), value)
         for geometry, value in zip(
@@ -21,14 +83,7 @@ def cbrfc_zone_mask(zone_raster, zone_shape):
         )
     )
 
-    return features.rasterize(
-        zone_geometries,
-        transform=transform,
-        out_shape=shape,
-        all_touched=True,
-        fill=MASK_NO_ZONE,
-        dtype=rasterio.int16
-    )
+    return rasterize_cbrfc_zone(zone_raster, zone_geometries)
 
 
 def zone_tif_lons_lats(zone_raster_file):
@@ -44,13 +99,12 @@ def zone_tif_lons_lats(zone_raster_file):
     return np.array(x), np.array(y)
 
 
-def cbrfc_zone_mask_as_xr(zone_raster, zone_shape):
-    zones_mask = cbrfc_zone_mask(zone_raster, zone_shape)
+def zone_mask_as_xr(zone_raster, zone_mask):
     lons, lats = zone_tif_lons_lats(zone_raster)
 
     cbrfc_zones = xr.Dataset({
         "zone": xr.DataArray(
-            data=zones_mask,
+            data=zone_mask,
             dims=['lat', 'lon'],
             coords={
                 'lat': (['lat'], lats[:, 0]),
@@ -62,3 +116,13 @@ def cbrfc_zone_mask_as_xr(zone_raster, zone_shape):
     cbrfc_zones = cbrfc_zones.where(cbrfc_zones['zone'] != MASK_NO_ZONE)
     # lat coordinates are flipped for some unknown reason
     return cbrfc_zones.reindex(lat=cbrfc_zones.lat[::-1])
+
+
+def cbrfc_zone_mask_as_xr(zone_raster, zone_shape):
+    zones_mask = cbrfc_zone_mask(zone_raster, zone_shape)
+    return zone_mask_as_xr(zone_raster, zones_mask)
+
+
+def single_cbrfc_zone_mask_as_xr(zone_raster, zone_gpd):
+    zones_mask = single_cbrfc_zone_mask(zone_raster, zone_gpd)
+    return zone_mask_as_xr(zone_raster, zones_mask)

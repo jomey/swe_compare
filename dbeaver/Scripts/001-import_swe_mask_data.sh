927bb4ab1df40ab60b3ecea114bf9ffc07c213b7
Joachim Meyer <jmeyer@boisestate.edu>
2024-04-17 11:04:02 -0500

    SQL - Fix import EPSG for SWANN SWE mask

--- dbeaver/Scripts/001-import_swe_mask_data.sh
+++ dbeaver/Scripts/001-import_swe_mask_data.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-raster2pgsql -I -C -e -Y -s 4629 -F -t 32x32 SWE_Mask_v01.nc swann_swe_mask | psql -h honduras -d swannData
+raster2pgsql -I -C -e -Y -s 4269 -F -t 32x32 SWE_Mask_v01.nc swann_swe_mask | psql -h honduras -d swannData

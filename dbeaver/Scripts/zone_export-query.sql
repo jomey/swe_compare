cc019b852fa8d9c0ebc3b8e49c1b519aa927d70a
Joachim Meyer <jmeyer@boisestate.edu>
2024-04-17 11:02:57 -0500

    SQL - Add sample query for zone envelope

--- dbeaver/Scripts/zone_export-query.sql
+++ dbeaver/Scripts/zone_export-query.sql
@@ -1,2 +1,6 @@
 SELECT ST_AsGDALRaster(ST_Union(zone_mask.rast), 'GTiff')
 FROM zone_mask_as_raster('ALEC2HUF') AS zone_mask;
+
+
+SELECT ST_ENVELOPE(zone_mask.rast) 
+FROM zone_mask_as_raster('ALEC2HUF') AS zone_mask;

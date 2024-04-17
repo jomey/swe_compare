3268996514d486432c5b19905fe195c75b05ecd5
Joachim Meyer <jmeyer@boisestate.edu>
2024-04-17 14:32:43 -0500

    src - ZoneCompare - Refactor to only handle two data sets
    Refactor the class to only hold the logic to compare two given datasets
    via time series and correlation plots. This moves handling bulk comparison
    logic back to the notebooks. The class does have two class methods that
    can handle showing many instances at once or save to a file.

--- src/swe_compare/zone_compare.py
+++ src/swe_compare/zone_compare.py
@@ -1,63 +1,139 @@
 import holoviews as hv
 import pandas as pd
 import panel as pn
+import xarray as xr
 
 from bokeh.resources import INLINE
 
 
 class ZoneCompare:
-    def __init__(self, target_zones, cbrfc_swe, swann_swe, year_range):
-        self.target_zones = target_zones
-        self.cbrfc_swe = cbrfc_swe
-        self.swann_swe = swann_swe
+    """
+    Generate time series and scatter plot for two datasets.
+    """
+
+    def __init__(
+        self,
+        zone_name: str,
+        snow_17: pd.Series,
+        swann: xr.DataArray,
+        year_range: range
+    ):
+        """
+        Parameters
+        ----------
+        zone_name : str
+            Zone name for plot title
+        snow_17 : pd.Series
+            Snow 17 data
+        swann : xr.DataArray
+            SWANN data
+        year_range : range
+            Range of years for correlation analysis
+        """
+        self.zone_name = zone_name
+        self.snow_17 = snow_17
+        self.swann = swann
         self.year_range = pd.to_datetime(
             [f'{year}-03-01' for year in year_range]
         )
 
-    def plot_panels(self, name):
-        zone_ID = self.target_zones[name]
+    def correlation_plot(self) -> hv.Layout:
+        """
+        Scatter plot with a 1:1 line
+
+        Returns
+        -------
+        hv.Layout
+        """
         axes_limits = (-20, 1000)
 
-        swann_data = self.swann_swe.sel(zone=zone_ID)
+        # Get requested date range
+        snow_17 = self.snow_17.loc[self.year_range]
+        # Convert to use pandas correlation functions
+        swann = self.swann.sel(time=self.year_range).to_pandas()
 
-        snow_17_m1 = self.cbrfc_swe.loc[self.year_range][name].values
-        swann_m1 = swann_data.loc[self.year_range].values
-
-        correlation = str(
-            f'{pd.Series(snow_17_m1).corr(pd.Series(swann_m1)):.3}'
+        # Calculate R^2
+        correlation_r2 = str(
+            f'{snow_17.corr(swann):.3}'
         )
 
-        scatter = hv.Overlay([
+        return hv.Overlay([
             hv.Slope(1, 0).opts(color='orange'),
             hv.Scatter(
-                list(zip(snow_17_m1, swann_m1))
+                list(zip(snow_17, swann))
             ).opts(
                 xlim=axes_limits, ylim=axes_limits,
-                title=f'{name} - March 1st SWE',  xlabel='CBRFC SWE (mm)',  ylabel='SWANN SWE (mm)',
+                title=f'{self.zone_name} - March 1st SWE',
+                xlabel='CBRFC SWE (mm)',  ylabel='SWANN SWE (mm)',
                 color='k', size=10,
                 width=500, height=500
-            ) * hv.Text(300, 10, correlation),
+            ) * hv.Text(300, 10, correlation_r2),
         ])
-        time_series = hv.Overlay([
-            self.cbrfc_swe[name].hvplot().opts(
+
+    def time_series_plot(self) -> hv.Overlay:
+        """
+        Create time series plot overlaying both datasets
+
+        Returns
+        -------
+        hv.Overlay
+        """
+        return hv.Overlay([
+            self.snow_17.hvplot().opts(
                 ylim=(-20, None),
-                title=name, ylabel='SWE (mm)',
+                title=self.zone_name, ylabel='SWE (mm)',
                 width=1200, height=600,
             ),
-            swann_data.hvplot('time', label=f'SWANN')
+            self.swann.hvplot('time', label='SWANN')
         ])
-        return hv.Layout(time_series + scatter).cols(1)
 
-    def show_all(self):
-        plots = [self.plot_panels(zone) for zone in self.target_zones.keys()]
+    def plot_all(self) -> hv.Layout:
+        """
+        Plot the time series and correlation side-by-side
+
+        Returns
+        -------
+        hv.Layout
+        """
+        return hv.Layout(
+            self.time_series_plot() + self.correlation_plot()
+        ).cols(2)
+
+    @classmethod
+    def show_all(cls, collection: list) -> hv.Layout:
+        """
+        Show collection of zone compare instances in one layout with two columns.
+
+        For use in notebooks.
+
+        Parameters
+        ----------
+        collection : list
+            Collection of ZoneCompare instances
+
+        Returns
+        -------
+        hv.Layout
+            Holoviews layout with all plots
+        """
+        plots = [zone_compare.plot_all() for zone_compare in collection]
         return hv.Layout(plots).opts(shared_axes=False).cols(2)
 
-    def save_html(self, file_path):
+    @classmethod
+    def save_html(cls, file_path: str, collection: list) -> None:
         """
         Save plots as interactive HTML page
+
+        Parameters
+        ----------
+        file_path : str
+            Path to save files to
+        collection : list
+            Collection of ZoneCompare instances
         """
+
         pn.pane.HoloViews(
-            self.show_all()
+            cls.show_all(collection)
         ).save(
             file_path, embed=True, resources=INLINE
         )

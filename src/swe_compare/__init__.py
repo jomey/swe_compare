20e010bcd71a019458924aa8452f23e09eb6da04
Joachim Meyer <jmeyer@boisestate.edu>
2024-04-17 10:55:22 -0500

    src - Init ZoneRaster class
    Class with methods to calculate the raster bounding box and to
    prepare adding the data as Xarry mask.

--- src/swe_compare/__init__.py
+++ src/swe_compare/__init__.py
@@ -2,10 +2,12 @@ from .CbrfcZone import CBRFCZone
 from .Snow17_SWE import Snow17SWE
 from .zone_compare import ZoneCompare
 from .zone_db import ZoneDB
+from .zone_raster import ZoneRaster
 
 __all__ = [
     CBRFCZone,
     Snow17SWE,
     ZoneCompare,
     ZoneDB,
+    ZoneRaster,
 ]
